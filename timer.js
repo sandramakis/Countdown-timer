178f27160e2f55bf6586a783087ff2ee118281e0
Sandra Nwakeze <sandramakis.dev@gmail.com>
2023-11-19 21:57:07 +0100

    ADD: Hour countdown

--- timer.js
+++ timer.js
@@ -17,9 +17,11 @@ function timer(seconds) {
 
   countdown = setInterval(() => {
     const secondsLeft = Math.floor((then - Date.now()) / 1000);
-    // const hoursLeft = Math.floor((then - Date.now()) / 3600000);
+    const hoursLeft = Math.floor((then - Date.now()) / 3600000);
+    const minutesLeft = Math.floor((then - Date.now()) / 60000);
+    console.log(minutesLeft);
 
-    if (secondsLeft < 0) {
+    if (secondsLeft < 0 && hoursLeft < 0) {
       clearInterval(countdown);
       return;
     }
@@ -28,11 +30,26 @@ function timer(seconds) {
   }, 1000);
 }
 
+// Change to 12 hour clock
+function hourClock(num) {
+  return num > 12 ? num - 12 : num;
+}
+
+// Add zero to numbers less than 10
+function addZero(num) {
+  return num < 10 ? `0${num}` : num;
+}
+
 // display seconds left from the first number
 function displayTimeLeft(seconds) {
-  const mins = Math.floor(seconds / 60);
+  let mins = Math.floor(seconds / 60);
   const secs = seconds % 60;
-  const display = `${mins} : ${secs < 10 ? "0" : ""}${secs}`;
+  const hoursRem = Math.floor(seconds / 3600);
+  mins = mins >= 60 ? mins % 60 : mins;
+
+  const display = `${hoursRem}: ${addZero(mins)} : ${addZero(secs)}`;
+
+  // display on the page
   displayTimer.textContent = display;
   document.title = display;
 }
@@ -42,9 +59,8 @@ function endTime(timestamp) {
   const end = new Date(timestamp);
   const hour = end.getHours();
   let mins = end.getMinutes();
-  const adjustedHour = hour > 12 ? hour - 12 : hour;
 
-  readyTime.textContent = `Be back at ${adjustedHour}:${mins < 10 ? 0 : mins}`;
+  readyTime.textContent = `Be back at ${addZero(hour)}:${addZero(mins)}`;
 }
 
 // hook the buttons up with the functions
